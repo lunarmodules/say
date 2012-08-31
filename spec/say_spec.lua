2c26ebd774a6db3ff5897fbd1bfc83cbc732ae7a
Thijs Schreijer <thijs@thijsschreijer.nl>
2012-08-31 17:28:10 +0200

    another silly error, I should stop doing that

--- spec/say_spec.lua
+++ spec/say_spec.lua
@@ -18,7 +18,7 @@ describe("Tests to make sure the say library is functional", function()
   end)
 
   it("tests the substitution of variable types; boolean, number, string and table", function()
-    s:set('substitute_test', 'boolean = %s, number = %s, string = "%s", nil = %s, table = %s')
+    s:set('substitute_test', 'boolean = %s, number = %s, string = "%s", table = %s')
     local atable = {}
     assert(s('substitute_test', {true, 100, 'some text', atable}) == 'boolean = true, number = 100, string = "some text", table = ' .. tostring(atable)
   end)
