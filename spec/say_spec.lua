7d30862a8f8a0a6c7dc7a76e8350d67dd712ca96
Thijs Schreijer <thijs@thijsschreijer.nl>
2012-08-31 13:57:44 +0200

    added test for variable substitutions

--- spec/say_spec.lua
+++ spec/say_spec.lua
@@ -17,6 +17,12 @@ describe("Tests to make sure the say library is functional", function()
     assert(s('herp', {'test', 'test'}) == 'testtest')
   end)
 
+  it("tests the substitution of variable types; boolean, number, string and table", function()
+    s:set('substitute_test', 'boolean = %s, number = %s, string = "%s", nil = %s, table = %s')
+    local atable = {}
+    assert(s('substitute_test', {true, 100, 'test', atable}) == 'boolean = true, number = 100, string = "some text", table = ' .. tostring(atable)
+  end)
+
   it("tests the set_fallback method", function()
     s:set_namespace('not-en')
     s:set('not-herp', 'not-derp')
